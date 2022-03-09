30d83146299e711ac8f897798f285c86714acdd9
Glen Pike <github@glenpike.co.uk>
2022-03-09 21:43:22 +0000

    Make it upload the directories

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 const listLocaleDirectories = async () => {
-  const filter = ["code", "data", "images", "dist", ".github", ".git"];
+  const filter = [
+    "code",
+    "data",
+    "images",
+    "dist",
+    ".github",
+    ".git",
+    "node_modules",
+  ];
   const files = await readdir("./", { withFileTypes: true });
   return files.reduce((list, fileInfo) => {
     if (fileInfo.isDirectory() && filter.indexOf(fileInfo.name) === -1) {
@@ -15,54 +23,55 @@ const listLocaleDirectories = async () => {
     return list;
   }, []);
 };
-const listLocaleMediaFiles = async (locale) => {
-  const mediaDirectories = ["images", "resources", "solutions"];
-  const mediaFiles = [];
-  for (let i = 0; i < mediaDirectories.length; i++) {
-    const files = await readdir(`./${locale}/${mediaDirectories[i]}`);
-    files.forEach((file) => {
-      mediaFiles.push(`${locale}/${mediaDirectories[i]}/${file}`);
-    });
-  }
-  return mediaFiles;
-};
 
-const uploadLocaleFiles = async (files) => {
+// const listLocaleMediaFiles = async (locale) => {
+//   const mediaDirectories = ["images", "resources", "solutions"];
+//   const mediaFiles = [];
+//   for (let i = 0; i < mediaDirectories.length; i++) {
+//     const files = await readdir(`./${locale}/${mediaDirectories[i]}`);
+//     files.forEach((file) => {
+//       mediaFiles.push(`${locale}/${mediaDirectories[i]}/${file}`);
+//     });
+//   }
+//   return mediaFiles;
+// };
+
+const uploadLocaleFiles = (directories) => {
   const aws = new awsCli.Aws();
-  const {
-    context: {
-      payload: {
-        after: commit,
-        repository: { name: repositoryName = "knowledge-quiz-test" },
-      },
-    },
-  } = github;
+  console.log("payload ", github.context.payload);
+  const commit = github.context.payload.after || "1234";
+  const { name: repositoryName } =
+    github.context.payload.repository || "knowledge-quiz-test";
 
-  // const repositoryName = "knowledge-quiz-test";
-  // const commit = "123";
   // `aws s3 cp ${FILE} s3://${path}/ \
   // --region ${process.env.AWS_REGION} $*`
   const path = `${process.env.AWS_BUCKET}/projects/${repositoryName}/${commit}`;
-  const file = "en/images/q1-1.png";
+  // const file = "en/images/q1-1.png";
 
-  const command = `aws s3 cp ./${file} s3://${path}/${file}`;
-  // console.log("aws copy ", path, file, command);
-  // const result = await aws.command(command);
-  const result = execSync(command);
-  console.log("aws copy result = ", result.toString());
+  directories.forEach((directory) => {
+    const command = `aws s3 cp ./${directory} s3://${path}/${directory} --recursive`;
+    const result = execSync(command);
+    console.log("aws copy result = ", result.toString());
+  });
 };
 
 async function main() {
   try {
     console.log("env: ", process.env);
     const filesToUpload = [];
-    // const localeDirectories = await listLocaleDirectories();
-    // for (let i = 0; i < localeDirectories.length; i++) {
-    //   const mediaFiles = await listLocaleMediaFiles(localeDirectories[i]);
-    //   mediaFiles.forEach((file) => filesToUpload.push(file));
-    // }
-    await uploadLocaleFiles();
-    core.setOutput("directories", filesToUpload);
+    const mediaDirectories = ["images", "resources", "solutions"];
+
+    const localeDirectories = await listLocaleDirectories();
+    for (let i = 0; i < localeDirectories.length; i++) {
+      mediaDirectories.forEach((directory) => {
+        filesToUpload.push(`${localeDirectories[i]}/${directory}`);
+      });
+      // const mediaFiles = await listLocaleMediaFiles(localeDirectories[i]);
+      // mediaFiles.forEach((file) => filesToUpload.push(file));
+    }
+    console.log("filesToUpload ", filesToUpload);
+    uploadLocaleFiles(filesToUpload);
+    core.setOutput("directories", []);
   } catch (error) {
     console.log("error ", error);
     core.setFailed(error.message);
